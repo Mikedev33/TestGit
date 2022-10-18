d9198cd3a15a1e8b4c8e0a34bac61766712ae376
Misha <michaelvy365@gmail.com>
2022-10-18 18:21:52 +0300

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by user on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
